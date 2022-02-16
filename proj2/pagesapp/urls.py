1576783cca336f4cf757c66c006d1f4a16bc77e9
SaidAbbos96 <abbos.xudoyqulov@mail.ru>
2022-02-16 13:50:41 +0500

    loaded files of blogsite

--- proj2/pagesapp/urls.py
+++ proj2/pagesapp/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', HomePageView.as_view(), name="home"),
-    path('about/', AboutPageView.as_view(), name="about"),
-    path('contacts/', ContactsPageView.as_view(), name="contacts"),
+    path('about', AboutPageView.as_view(), name="about"),
+    path('contacts', ContactsPageView.as_view(), name="contacts"),
 ]
