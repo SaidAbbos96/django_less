b446b082b63edc671c2c4c14b9f3ed7096489d2d
SaidAbbos96 <abbos.xudoyqulov@mail.ru>
2022-02-16 20:04:05 +0500

    upload

--- proj3/news/models.py
+++ proj3/news/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=150)
     text = models.TextField()
+    author = models.CharField(max_length=30)
 
     def __str__(self):
         return self.title
