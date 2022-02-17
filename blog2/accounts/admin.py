9578f09014146455a34bae4b44b77e23183bf693
SaidAbbos96 <abbos.xudoyqulov@mail.ru>
2022-02-17 10:12:07 +0500

    created now blog2

--- blog2/accounts/admin.py
+++ blog2/accounts/admin.py
@@ -9,6 +9,13 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
+    list_display = ['username', 'first_name', 'last_name', 'email', 'age', 'is_staff']
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('age',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('age',)}),
+    )
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
